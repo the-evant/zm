6d7affcb7b08a721a5d8112341179cf09e6ec870
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-27 23:39:01 +0200

    feat: Created base plugin.

---
+++ src/main/java/fr/shuvly/zm/Zm.java
@@ -0,0 +1,21 @@
+package fr.shuvly.zm;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Zm
+    extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // Plugin shutdown logic
+    }
+
+}
