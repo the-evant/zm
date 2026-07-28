ec0a822246142aaf894a56a5abcdba0c2c5a7837
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-28 14:07:46 +0200

    feat(command): Created CommandManager.

---
+++ src/main/java/fr/shuvly/zm/command/CommandManager.java
@@ -0,0 +1,23 @@
+package fr.shuvly.zm.command;
+
+import fr.shuvly.core.common.command.AbstractCommandManager;
+import fr.shuvly.zm.command.dev.zone.ZoneCommand;
+import org.bukkit.plugin.PluginManager;
+
+public class CommandManager
+    extends AbstractCommandManager
+{
+
+    public CommandManager(PluginManager pluginManager)
+    {
+        super(pluginManager);
+    }
+
+
+    @Override
+    protected void registerCommands(PluginManager pluginManager)
+    {
+        super.registerCommand("zone", ZoneCommand.class);
+    }
+
+}
