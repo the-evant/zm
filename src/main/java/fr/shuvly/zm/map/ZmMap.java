7ee1e7bfd297cacdc4fcf8f59f7f0e69baafc543
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-29 14:08:50 +0200

    feat(map): Added a Component registry in ZmMap class.

--- src/main/java/fr/shuvly/zm/map/ZmMap.java
+++ src/main/java/fr/shuvly/zm/map/ZmMap.java
@@ -2,6 +2,8 @@ package fr.shuvly.zm.map;
 
 import org.bukkit.Location;
 
+import fr.shuvly.zm.component.ComponentRegistry;
+
 import java.util.List;
 import java.util.Map;
 
@@ -11,17 +13,20 @@ public class ZmMap
     private final ZmMapInfo info;
     private final List<Location> spawnPoints;
     private final Map<String, Zone> zones;
+    private final ComponentRegistry componentRegistry;
 
 
     public ZmMap(
         ZmMapInfo info,
         List<Location> spawnPoints,
-        Map<String, Zone> zones
+        Map<String, Zone> zones,
+        ComponentRegistry componentRegistry
     )
     {
         this.info = info;
         this.spawnPoints = spawnPoints;
         this.zones = zones;
+        this.componentRegistry = componentRegistry;
     }
 
 
@@ -38,5 +43,6 @@ public class ZmMap
 
     public ZmMapInfo getInfo() { return info; }
     public List<Location> getSpawnPoints() { return spawnPoints; }
+    public ComponentRegistry getComponentRegistry() { return componentRegistry; }
 
 }
