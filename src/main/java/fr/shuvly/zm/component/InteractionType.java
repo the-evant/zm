8d119ea5db8570dae89d2870940228310dc610f9
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-29 14:01:16 +0200

    feat(component): Created `InteractionType` enum.

---
+++ src/main/java/fr/shuvly/zm/component/InteractionType.java
@@ -0,0 +1,12 @@
+package fr.shuvly.zm.component;
+
+public enum InteractionType
+{
+
+    RIGHT_CLICK,
+    LEFT_CLICK,
+    SNEAK,
+    SWAP_HAND,  // 'F' by default
+    DROP        // 'Q' by default
+
+}
