82310328069e4cb6963432179b0766fbce76f12b
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-29 13:32:42 +0200

    feat(map): Created `ZmMapInfo` DTO.

---
+++ src/main/java/fr/shuvly/zm/map/ZmMapInfo.java
@@ -0,0 +1,8 @@
+package fr.shuvly.zm.map;
+
+public record ZmMapInfo(
+    String name,        // "nacht"
+    String displayName, // "Nacht der Untoten"
+    String worldName,   // "zm_nacht"
+    String configPath   // "zm_nacht/zm_nacht.yml"
+) {}
