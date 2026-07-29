1d424796fdf85f813e6c5265104cdb9abc2448b0
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-29 14:07:23 +0200

    feat(component): Drafted Interactable interface.

---
+++ src/main/java/fr/shuvly/zm/component/Interactable.java
@@ -0,0 +1,11 @@
+package fr.shuvly.zm.component;
+
+import fr.shuvly.zm.player.ZmPlayer;
+
+public interface Interactable
+{
+
+    boolean onInteract(ZmPlayer player, InteractionType interactionType);
+    String getPromptText(ZmPlayer player);
+
+}
