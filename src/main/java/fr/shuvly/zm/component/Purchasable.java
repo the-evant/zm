a438d2ad1f4700101069235d17e43f65de68eec5
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-29 14:07:35 +0200

    feat(component): Drafted Purchasable interface.

---
+++ src/main/java/fr/shuvly/zm/component/Purchasable.java
@@ -0,0 +1,24 @@
+package fr.shuvly.zm.component;
+
+import fr.shuvly.zm.player.ZmPlayer;
+
+public interface Purchasable
+    extends Interactable
+{
+
+    int getCost();
+    void onPurchase(ZmPlayer player);
+
+    @Override
+    default boolean onInteract(ZmPlayer player, InteractionType interactionType)
+    {
+        return true;
+//        if (player.hasPoints(getCost())) {
+//            player.removePoints(getCost());
+//            onPurchase(player);
+//        } else {
+//            // TODO: Play "not enough points" sound
+//        }
+    }
+
+}
