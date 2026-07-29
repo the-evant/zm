d5fdf8b59715976c219b6e799fc7f257f5b85a07
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-29 14:07:52 +0200

    feat(component>door): Created DoorComponent (still under work though).

---
+++ src/main/java/fr/shuvly/zm/component/door/DoorComponent.java
@@ -0,0 +1,81 @@
+package fr.shuvly.zm.component.door;
+
+import fr.shuvly.zm.component.BaseComponent;
+import fr.shuvly.zm.component.Purchasable;
+import fr.shuvly.zm.map.region.Region;
+import fr.shuvly.zm.map.Zone;
+import fr.shuvly.zm.player.ZmPlayer;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class DoorComponent
+    extends BaseComponent
+    implements Purchasable
+{
+
+    private final DoorType type;
+    private final int cost;
+    private final Set<Zone> targetZones;
+
+    private boolean isOpened = false;
+
+
+    public DoorComponent(String id, Region region, DoorType type, int cost)
+    {
+        super(id, region);
+        this.type = type;
+        this.cost = cost;
+        this.targetZones = new HashSet<>();
+    }
+
+
+    public void addTargetZone(Zone zone)
+    {
+        this.targetZones.add(zone);
+    }
+    public void addTargetZones(Set<Zone> zones)
+    {
+        this.targetZones.addAll(zones);
+    }
+
+    @Override
+    public int getCost()
+    {
+        return cost;
+    }
+
+    @Override
+    public void onPurchase(ZmPlayer player)
+    {
+        if (isOpened) {
+            return;
+        }
+
+        this.isOpened = true;
+
+        for (Zone zone : targetZones) {
+            zone.setUnlocked(true);
+        }
+
+        // TODO: Play door open animation (remove blocks) and sound
+    }
+
+    @Override
+    public String getPromptText(ZmPlayer player)
+    {
+        if (isOpened) {
+            return null;
+        }
+
+        final String action = (type == DoorType.DOOR) ? "open door" : "clear debris";
+        return "Press [F] to " + action + " [Cost: " + cost + "]";
+    }
+
+
+    public boolean isOpened() { return isOpened; }
+    public DoorType getType() { return type; }
+    public Set<Zone> getTargetZones() { return Collections.unmodifiableSet(targetZones); }
+
+}
