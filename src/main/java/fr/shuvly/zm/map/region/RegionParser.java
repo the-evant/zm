79d3ac61dd8689a28fdbf2c9cfe4f39489f6cc7a
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-29 13:32:15 +0200

    ref(map>region): Moved RegionParser into `region/` folder.

--- src/main/java/fr/shuvly/zm/map/region/RegionParser.java
+++ src/main/java/fr/shuvly/zm/map/region/RegionParser.java
@@ -1,7 +1,6 @@
-package fr.shuvly.zm.parser;
+package fr.shuvly.zm.map.region;
 
 import fr.shuvly.zm.exception.MapParseException;
-import fr.shuvly.zm.map.region.Region;
 import fr.shuvly.zm.map.region.regions.CompositeRegion;
 import fr.shuvly.zm.map.region.regions.CuboidRegion;
 import fr.shuvly.zm.map.region.regions.CylinderRegion;
@@ -12,7 +11,6 @@ import org.bukkit.util.BoundingBox;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 public class RegionParser
 {
@@ -37,19 +35,33 @@ public class RegionParser
                     throw new MapParseException("CUBOID region is missing 'min' or 'max' coordinates.");
                 }
 
+                double x1 = min.getDouble("x");
+                double y1 = min.getDouble("y");
+                double z1 = min.getDouble("z");
+
+                double x2 = max.getDouble("x");
+                double y2 = max.getDouble("y");
+                double z2 = max.getDouble("z");
+
                 yield new CuboidRegion(
                     new BoundingBox(
-                        min.getDouble("x"), min.getDouble("y"), min.getDouble("z"),
-                        max.getDouble("x"), max.getDouble("y"), max.getDouble("z")
+                        Math.min(x1, x2), Math.min(y1, y2), Math.min(z1, z2),
+                        Math.max(x1, x2) + 1.0, Math.max(y1, y2) + 1.0, Math.max(z1, z2) + 1.0
                     )
                 );
             }
 
-            case "CYLINDER" -> CylinderRegion.fromRadius(
-                section.getDouble("center_x"), section.getDouble("center_z"),
-                section.getDouble("radius"),
-                section.getDouble("min_y"), section.getDouble("max_y")
-            );
+            case "CYLINDER" -> {
+                double y1 = section.getDouble("min_y");
+                double y2 = section.getDouble("max_y");
+
+                yield CylinderRegion.fromRadius(
+                    section.getDouble("center_x") + 0.5,
+                    section.getDouble("center_z") + 0.5,
+                    section.getDouble("radius"),
+                    Math.min(y1, y2), Math.max(y1, y2) + 1.0
+                );
+            }
 
             case "COMPOSITE" -> {
                 final List<Map<?, ?>> partsList = section.getMapList("parts");
