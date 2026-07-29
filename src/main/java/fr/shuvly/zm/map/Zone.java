c664d37e33470567d06b2ccfd276436bda86bc99
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-29 13:57:36 +0200

    feat(map>zone): Added neighbors (graph).

--- src/main/java/fr/shuvly/zm/map/Zone.java
+++ src/main/java/fr/shuvly/zm/map/Zone.java
@@ -3,12 +3,17 @@ package fr.shuvly.zm.map;
 import fr.shuvly.zm.map.region.Region;
 import org.bukkit.Location;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Zone
 {
 
     private final String id;
     private final Region region;
     private boolean isUnlocked;
+    private final Set<Zone> neighbours = new HashSet<>();
 
 
     public Zone(
@@ -28,11 +33,12 @@ public class Zone
         return region.contains(location.getX(), location.getY(), location.getZ());
     }
 
-
+    public void addAdjacentZone(Zone zone) { this.neighbours.add(zone); }
     public void setUnlocked(boolean unlocked) { this.isUnlocked = unlocked; }
 
     public String getId() { return id; }
     public Region getRegion() { return region; }
     public boolean isUnlocked() { return isUnlocked; }
+    public Set<Zone> getNeighbours() { return Collections.unmodifiableSet(neighbours); }
 
 }
