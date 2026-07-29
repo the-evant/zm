1656cd393f76fbc1731a1402f6644c382f1747e4
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-29 14:10:23 +0200

    feat(command): Created "/game" command.

--- src/main/java/fr/shuvly/zm/command/CommandManager.java
+++ src/main/java/fr/shuvly/zm/command/CommandManager.java
@@ -1,6 +1,7 @@
 package fr.shuvly.zm.command;
 
 import fr.shuvly.core.common.command.AbstractCommandManager;
+import fr.shuvly.zm.command.dev.game.GameCommand;
 import fr.shuvly.zm.command.dev.zone.ZoneCommand;
 import org.bukkit.plugin.PluginManager;
 
@@ -18,6 +19,7 @@ public class CommandManager
     protected void registerCommands(PluginManager pluginManager)
     {
         super.registerCommand("zone", ZoneCommand.class);
+        super.registerCommand("game", GameCommand.class);
     }
 
 }
