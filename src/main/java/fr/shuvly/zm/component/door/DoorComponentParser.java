c361def01003314a5ddea495c7dfd21084b8524a
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-29 14:07:56 +0200

    feat(component>door): Created DoorComponent parser.

---
+++ src/main/java/fr/shuvly/zm/component/door/DoorComponentParser.java
@@ -0,0 +1,81 @@
+package fr.shuvly.zm.component.door;
+
+import fr.shuvly.zm.component.ComponentRegistry;
+import fr.shuvly.zm.exception.MapParseException;
+import fr.shuvly.zm.map.Zone;
+import fr.shuvly.zm.map.region.Region;
+import fr.shuvly.zm.map.region.RegionParser;
+import org.bukkit.configuration.ConfigurationSection;
+
+import java.util.List;
+import java.util.Map;
+
+public class DoorComponentParser
+{
+
+    private DoorComponentParser() {}
+
+
+    public static void parse(
+        ConfigurationSection doorsSection,
+        Map<String, Zone> zones,
+        ComponentRegistry registry
+    )
+        throws MapParseException
+    {
+        if (doorsSection == null) {
+            return;
+        }
+
+        for (String doorId : doorsSection.getKeys(false)) {
+            final ConfigurationSection sec = doorsSection.getConfigurationSection(doorId);
+
+            if (sec == null) {
+                continue;
+            }
+
+            final List<String> connects = sec.getStringList("connects");
+            final ConfigurationSection regionSec = sec.getConfigurationSection("region");
+
+            if (regionSec == null) {
+                throw new MapParseException("DOOR '" + doorId + "' region has not been found.");
+            }
+
+            final Region region = RegionParser.parse(regionSec);
+
+            final int cost = sec.getInt("cost", 0); // default price of doors : 0 (free)
+            final String typeStr = sec.getString("type", "DOOR").toUpperCase();
+            
+            final DoorType type;
+
+            try {
+                type = DoorType.valueOf(typeStr);
+            } catch (IllegalArgumentException e) {
+                throw new MapParseException("DOOR '" + doorId + "': Unknown door type: " + typeStr);
+            }
+
+            final DoorComponent door = new DoorComponent(doorId, region, type, cost);
+
+            for (String zoneId : connects) {
+                final Zone zone = zones.get(zoneId);
+
+                if (zone != null) {
+                    door.addTargetZone(zone);
+                } else {
+                    throw new MapParseException("DOOR '" + doorId + "': Zone '" + zoneId + "' does not exist.");
+                }
+            }
+
+            for (Zone z1 : door.getTargetZones()) {
+                for (Zone z2 : door.getTargetZones()) {
+                    if (z1 != z2) {
+                        z1.addAdjacentZone(z2);
+                    }
+                }
+            }
+
+            registry.register(door);
+        }
+    }
+
+}
