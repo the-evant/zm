5d0f2daa2875a646391342d26ca572b75e9adbd7
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-30 23:13:37 +0200

    feat(map>region): Added function to raytrace into the region.

--- src/main/java/fr/shuvly/zm/map/region/Region.java
+++ src/main/java/fr/shuvly/zm/map/region/Region.java
@@ -1,5 +1,8 @@
 package fr.shuvly.zm.map.region;
 
+import org.bukkit.util.RayTraceResult;
+import org.bukkit.util.Vector;
+
 public interface Region
 {
 
@@ -7,5 +10,10 @@ public interface Region
      * Evaluates if the given coordinates fall inside this region.
      */
     boolean contains(double x, double y, double z);
+
+    /**
+     * Performs a mathematical raytrace against this region's geometry.
+     */
+    RayTraceResult rayTrace(Vector start, Vector direction, double maxDistance);
 
 }
