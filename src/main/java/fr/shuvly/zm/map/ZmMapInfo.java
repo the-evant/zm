39a3052fcf57bd0719e061cf2e52a6cf13765771
Lysandre B. <lysandre.boursette@epitech.eu>
2026-07-31 18:02:45 +0200

    feat(map>info): Now automatically computes world name.

--- src/main/java/fr/shuvly/zm/map/ZmMapInfo.java
+++ src/main/java/fr/shuvly/zm/map/ZmMapInfo.java
@@ -1,8 +1,23 @@
 package fr.shuvly.zm.map;
 
 public record ZmMapInfo(
-    String name,        // "nacht"
+    String id,          // "nacht"
     String displayName, // "Nacht der Untoten"
-    String worldName,   // "zm_nacht"
-    String configPath   // "zm_nacht/zm_nacht.yml"
-) {}
+    String worldName,   // "zm_nacht" ; automatically resolved
+    String configPath   // "plugins/maps/zm_nacht/zm_nacht.yml"
+)
+{
+
+    public ZmMapInfo
+    {
+        if (!worldName.equals("zm_" + id)) {
+            throw new IllegalArgumentException("worldName must exactly match 'zm_' + id");
+        }
+    }
+
+    public ZmMapInfo(String name, String displayName, String configPath)
+    {
+        this(name, displayName, "zm_" + name, configPath);
+    }
+
+}
