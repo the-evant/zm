67917ff251120322bb70fb14d3aaf956734a7ad8
Lysandre B. <lysandre.boursette@epitech.eu>
2026-08-05 12:40:11 +0200

    feat(player): Added points system for players. Default value is 0.

--- src/main/java/fr/shuvly/zm/player/ZmPlayer.java
+++ src/main/java/fr/shuvly/zm/player/ZmPlayer.java
@@ -5,18 +5,33 @@ import org.bukkit.entity.Player;
 public class ZmPlayer
 {
 
-    private Player player;
+    private final Player player;
     private ZmPlayerState state;
+    private int points;
 
 
     public ZmPlayer(Player player)
     {
         this.player = player;
         this.state = ZmPlayerState.ALIVE;
+        this.points = 0;
     }
 
 
     public Player getPlayer() { return player; }
+
+    public void setState(ZmPlayerState state) { this.state = state; }
     public ZmPlayerState getState() { return state; }
+
+    public int getPoints() { return points; }
+    public void addPoints(int points) { this.points += points; }
+    public boolean removePoints(int points)
+    {
+        if (points < this.points) {
+            this.points -= points;
+            return true;
+        }
+        return false;
+    }
 
 }
