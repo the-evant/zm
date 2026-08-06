25752ccf27590cf5158f6b4651b033c49e61b502
Lysandre B. <lysandre.boursette@epitech.eu>
2026-08-06 16:34:45 +0200

    fix(dep/build): Fixed wrong package name for `madgui` library.

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
     compileOnly("fr.shuvly:core:1.0-D")
-    implementation("fr.shuvly.paper:madgui:1.0-D")
+    implementation("fr.shuvly:madgui:1.0-D")
 }
 
 tasks {
