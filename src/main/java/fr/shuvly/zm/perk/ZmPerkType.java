b1a54099bd7eb3905d3953b6f3550d1ac5f2678d
Lysandre B. <lysandre.boursette@epitech.eu>
2026-08-16 15:22:43 +0200

    feat(perk): Created `ZmPerkType` enum with 5 base perks.

--- src/main/java/fr/shuvly/zm/perk/ZmPerkType.java
+++ src/main/java/fr/shuvly/zm/perk/ZmPerkType.java
@@ -1,4 +1,24 @@
 package fr.shuvly.zm.perk;
 
-public enum ZmPerkType {
+public enum ZmPerkType
+{
+
+    JUGGERNOG,
+    MULE_KICK,
+    SPEED_COLA,
+    STAMINA_UP,
+    DOUBLE_TAP;
+
+
+    private final String id;
+
+
+    ZmPerkType()
+    {
+        this.id = "perk_" + name().toLowerCase();
+    }
+
+
+    public String getId() { return id; }
+
 }
